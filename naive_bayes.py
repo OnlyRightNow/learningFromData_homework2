c809ad8062ae4940385209dc46beeffb3d26418d
Sebastian <yodaxx3@gmail.com>
2019-10-24 16:37:20 +0800

    + vetorize it (for being super fast:D)

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Zifeng Wang"
-__email__  = "wangzf18@mails.tsinghua.edu.cn"
-__date__   = "20191005"
+__email__ = "wangzf18@mails.tsinghua.edu.cn"
+__date__ = "20191005"
 
 import numpy as np
 import pdb
 from sklearn.naive_bayes import BernoulliNB
 
 np.random.seed(2019)
+
 
 class BernoulliNaiveBayes:
     def __init__(self):
@@ -27,29 +28,19 @@ class BernoulliNaiveBayes:
         """
         Please Fill Your Code Here.
         """
-
         # learn the p(y=1) and p(y=0)
         m = y_train.shape[0]
-        self.p_y1 = np.sum(y_train == 1)/m
-        self.p_y0 = np.sum(y_train == 0)/m
-
+        self.p_y1 = np.sum(y_train == 1) / m
+        self.p_y0 = np.sum(y_train == 0) / m
 
         # learn the p(x|y=0), p(x|y=1), they should be a vector with shape (# of feature, ).
         matrix_y_train = np.array([y_train, ] * x_train.shape[1]).transpose()
         xy1 = np.logical_and(x_train == 1, matrix_y_train == 1)
         y1 = (y_train == 1)
-        self.p_x_y1 = (np.sum(xy1, axis=0) + 1)/(np.sum(y1) + 2)
+        self.p_x_y1 = (np.sum(xy1, axis=0) + 1) / (np.sum(y1) + 2)
         xy0 = np.logical_and(x_train == 1, matrix_y_train == 0)
         y0 = (y_train == 0)
-        self.p_x_y0 = (np.sum(xy0, axis=0) + 1)/(np.sum(y0) + 2)
-
-
-        # xy1 = np.logical_and(x_train[:, j] == 1, y_train == 1)
-        # y1 = (y_train == 1)
-        # self.p_x_y1 = (sum(xy1) + 1) / (sum(y1) + 2)
-        # xy0 = np.logical_and(x_train[:, j] == 1, y_train == 0)
-        # y0 = (y_train == 0)
-        # self.p_x_y0 = (sum(xy0) + 1) / (sum(y0) + 2)
+        self.p_x_y0 = (np.sum(xy0, axis=0) + 1) / (np.sum(y0) + 2)
 
     def predict(self, x_test):
         """Do prediction via the learnt model.
@@ -64,24 +55,22 @@ class BernoulliNaiveBayes:
         """
         Please Fill Your Code Here.
         """
+        p_y0_x_array = np.log(self.p_y0) + np.sum(np.log(
+                x_test * (np.array([self.p_x_y0, ]*x_test.shape[0])) + (np.ones(x_test.shape) - np.array([self.p_x_y0, ]*x_test.shape[0])) * (
+                            np.ones(x_test.shape) - x_test)), axis=1)
+        p_y1_x_array = np.log(self.p_y1) + np.sum(np.log(
+                x_test * (np.array([self.p_x_y1, ]*x_test.shape[0])) + (np.ones(x_test.shape) - np.array([self.p_x_y1, ]*x_test.shape[0])) * (
+                            np.ones(x_test.shape) - x_test)), axis=1)
 
-        # do inference to obtain p(y=0|x) and p(y=1|x),
-        # then compare them to get the prediction.
-
-        p_y0_x = np.log(self.p_y0) + np.sum(np.log(self.p_x_y0)) - (np.sum(self.p_x_y1*self.p_y1) + np.sum(self.p_x_y0*self.p_y0))
-        p_y1_x = np.log(self.p_y1) + np.sum(np.log(self.p_x_y1)) - (np.sum(self.p_x_y1*self.p_y1) + np.sum(self.p_x_y0*self.p_y0))
-
-        if p_y0_x > p_y1_x:
-            pred = 1
-        else:
-            pred = 0
+        pred = np.greater(p_y1_x_array, p_y0_x_array)
         return pred
+
 
 def load_data(data_path="a1a.txt"):
     labels = []
     x = None
     with open(data_path, "r") as f:
-        for i,line in enumerate(f.readlines()):
+        for i, line in enumerate(f.readlines()):
             if i % 200 == 0:
                 print("Processing line No.{}.".format(i))
             data_list = line.split()
@@ -94,7 +83,7 @@ def load_data(data_path="a1a.txt"):
             if x is None:
                 x = features
             else:
-                x = np.c_[x,features]
+                x = np.c_[x, features]
 
     x = x.T
     labels = np.array(labels).astype(int)
@@ -102,17 +91,18 @@ def load_data(data_path="a1a.txt"):
     np.random.shuffle(all_idx)
     x_train = x[all_idx[:1000]]
     y_train = labels[all_idx[:1000]]
-    x_test  = x[all_idx[1000:]]
-    y_test  = labels[all_idx[1000:]]
+    x_test = x[all_idx[1000:]]
+    y_test = labels[all_idx[1000:]]
 
     return x_train, y_train, x_test, y_test
 
+
 if __name__ == '__main__':
     x_train, y_train, x_test, y_test = load_data()
-    print("# of Training data:",x_train.shape[0])
-    print("# of Test data:",x_test.shape[0])
+    print("# of Training data:", x_train.shape[0])
+    print("# of Test data:", x_test.shape[0])
 
-    acc_func = lambda x,y: 100 * (x == y).sum() / y.shape[0]
+    acc_func = lambda x, y: 100 * (x == y).sum() / y.shape[0]
 
     clf = BernoulliNaiveBayes()
 
